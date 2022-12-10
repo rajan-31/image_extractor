22d76526dfdcc04a4c936914ee45438cc0b2f0e7
Rajan Khade <rajankhade31@gmail.com>
2022-12-11 04:49:14 +0530

    add proxy url option

--- main.js
+++ main.js
@@ -1,28 +1,72 @@
+function verify_img_extension(str) {
+    if (str.endsWith('.jpg')) return true
+    else if (str.endsWith('.jpeg')) return true
+    else if (str.endsWith('.png')) return true
+    else if (str.endsWith('.svg')) return true
+    else if (str.endsWith('.webp')) return true
+    else return false
+}
+
 $(document).ready(function () {
 
     // handle extract btn click
     $('#extract_btn').click(function () {
-        const url = $('#url').val();
+        const proxy_url = $('#proxy').val();
+        const proxy_for = $("#proxy_for").val();
+        const sanitize_scripts = true;
+
+        const url = proxy_url == "" || proxy_for == 1 ? $('#url').val() : proxy_url + $('#url').val();
+        console.log("Main URL: " + url);
+
+
         if (url != "") {
             $('#user_msg').html('wait...');
 
         axios.get(url)
-            .then(function (response) {
+            .then(async function (response) {
                 // handle success
                 console.log(`Response Status: ${response.status}`);
-                $(`<div id="extracted_html">${response.data}</div>`, {}).appendTo("body");
+
+                //------------------------------------------------
+                let temp_html = $.parseHTML(response.data);
+
+                console.log(temp_html);
+                let sanitized_html = temp_html.filter((elt) => {
+                    if (sanitize_scripts == true && elt.tagName == 'SCRIPT') return false;
+                    if ((elt.tagName == 'LINK' && elt.rel == 'stylesheet') || elt.tagName == 'STYLE') return false;
+                    else return true;
+                })
+                console.log(sanitized_html);
+                //------------------------------------------------
+
+                $('#extracted_html').append(sanitized_html);
 
                 // get image urls
                 let e_all = $('#extracted_html').find('img').map(function () { return this.src; }).get();
 
                 // get pure urls
                 let e_pure = [];
+
+                // use proxy url if set in select
+                let e_proxy_temp = proxy_for == 1 || proxy_for == 2 ? proxy_url : "";
                 e_all.forEach(element => {
-                    e_pure.push(element.split("?")[0]);
+                    let temp1 = element.split("?")[0];
+                    if (verify_img_extension(temp1))
+                        e_pure.push(e_proxy_temp + temp1);
                 });
                 console.log(e_pure);
-                // e_pure = e_pure.slice(10, 15)
 
+                //--------------------------------------------------------------------
+                // const delay = $('#delay').val();
+                // console.log(`Start delay for ${delay} seconds.`);
+                // $('#user_msg').html('Main URL fetched delaying image fetch...');
+
+                // await setTimeout(() => {
+                //     console.log(`Delayed for ${delay} seconds.`);
+                //     $('#user_msg').html('wait...');
+                // }, delay * 1000)
+
+                //--------------------------------------------------------------------
                 // download and zip images
                 console.log('------download and zip images------')
                 var zip = new JSZip();
@@ -31,10 +75,18 @@ $(document).ready(function () {
                 e_pure.forEach(element => {
                     console.log(element);
 
-                    const imageBlob = fetch(element).then(response => response.blob());
+                    const imageBlob = fetch(element).then(response => response.blob()).catch((error) => {
+                        console.log("IMAGE FETCH ERROR for: " + element + "====> " + error)
+                    });
+                    ;
                     let temp_name = element.split('/');
                     temp_name = temp_name[temp_name.length - 1];
-                    zip.file(temp_name, imageBlob);
+
+                    try {
+                        zip.file(temp_name, imageBlob);
+                    } catch (error) {
+                        console.log('ERROR ADDING FILE TO ZIP ====> ' + error)
+                    }
                 });
 
                 zip.generateAsync({ type: "blob" })
@@ -50,7 +102,7 @@ $(document).ready(function () {
             })
             .catch(function (error) {
                 // handle error
-                console.log(error);
+                console.log("MAIN FETCH ERROR ====> " + error);
                 $('#user_msg').html(error.message);
             })
             .then(function () {
